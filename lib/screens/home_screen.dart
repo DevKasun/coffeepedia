6f47acec974f1f2a68416decbe841fe048b5d82f
Kasun Lakshitha <kasun2014@outlook.com>
2023-02-06 02:30:04 +0530

    Carousel added to show coffee data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 import 'package:http/http.dart' as http;
 
@@ -18,10 +19,11 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Colors.brown[600],
+        backgroundColor: Colors.brown[400],
         title: Text("CoffeePedia"),
         actions: [
           IconButton(
@@ -32,42 +34,50 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: coffeeList.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                print("Click Coffee card");
-              },
-              child: Card(
-                color: Colors.brown[300],
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    children: [
-                      Image.network(coffeeList[index]["image"],
-                          width: 150, height: 150, fit: BoxFit.cover),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(
-                          coffeeList[index]["title"],
-                          style: TextStyle(fontSize: 22.0),
-                        ),
+        child: CarouselSlider(
+          options: CarouselOptions(
+            height: height,
+            viewportFraction: 1.0,
+            enlargeCenterPage: false,
+          ),
+          items: coffeeList.map(
+            (coffee) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    decoration: BoxDecoration(color: Colors.brown[100]),
+                    child: Container(
+                      margin: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Image.network(
+                            coffee['image'],
+                            width: double.infinity,
+                            height: 300,
+                            fit: BoxFit.cover,
+                          ),
+                          SizedBox(height: 20.0),
+                          Text(
+                            coffee['title'],
+                            style: TextStyle(fontSize: 24.0),
+                          ),
+                          SizedBox(height: 20.0),
+                          Text(coffee['description'],
+                              style: TextStyle(fontSize: 14.0))
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
+                    ),
+                  );
+                },
+              );
+            },
+          ).toList(),
         ),
       ),
     );
   }
 
   _fetchUsers() async {
-    print("Fetch users called");
-
     const url = "https://api.sampleapis.com/coffee/hot";
     final uri = Uri.parse(url);
     final response = await http.get(uri);
